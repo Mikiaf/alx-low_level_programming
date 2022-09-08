94266df48520ba6cb4e67d47603874f9d1aceb10
Mikiaf <mikiyasafework12@gmail.com>
2022-09-08 16:24:49 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main-print string in thr put function
+*
+*Description : this program print "Programming is like building a multilingual puzzle,"
+*Return:0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle,");
+	return (0);
+}
