22b757d72642b996d83228197f5d57eb71ba6c12
Mikiaf <mikiyasafework12@gmail.com>
2022-09-08 15:41:46 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+* printing  with proper grammar, but the outcome is a piece of art,with printf
+* use main function
+*and return 0
+*/
 int main(void)
 {
 	printf ("with proper grammar, but the outcome is a piece of art,\n");
