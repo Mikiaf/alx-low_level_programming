006a28cc1c1e7466ba37b8b7a497dd88650e454e
Mikiaf <mikiyasafework12@gmail.com>
2022-09-12 15:30:05 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - print the string in the puts function
+*
+* Description:using the main function
+* this program print n
+* Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++ )
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
