0dd51de0063170700e88a0801140c9423c5badd6
Mikiaf <mikiyasafework12@gmail.com>
2022-09-12 16:12:15 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - print the string in the puts function
+*
+* Description:using the main function
+* this program print n
+* Return: 0
+*/
+int main (void)
+{
+	int num;
+
+	for(num = 0; num < 10 ; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
+
