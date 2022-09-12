76d4704f90ced8cb09a57b05b1f6ec20a8cca4bd
Mikiaf <mikiyasafework12@gmail.com>
2022-09-12 16:01:54 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - print the string in the puts function
+*
+* Description:using the main function
+* this program print n
+* Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
