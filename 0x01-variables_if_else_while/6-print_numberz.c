b84b0fd921fc06bdf17d07ff155bd7163343ca2b
Mikiaf <mikiyasafework12@gmail.com>
2022-09-12 16:48:41 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,11 @@
 * this program print n
 * Return: 0
 */
-int main (void)
+int main(void)
 {
 	int num;
 
-	for(num = 0; num < 10 ; num++)
+	for (num = '0'; num <= '9' ; num++)
 	{
 		putchar(num);
 	}
