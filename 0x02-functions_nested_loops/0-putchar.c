79d87d759b76a62d31f0c6f8935685bf96eba649
Mikiaf <mikiyasafework12@gmail.com>
2022-09-13 02:51:56 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print the string in the puts function
+*
+* Description:using the main function
+* this program print n
+* Return: 0
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
