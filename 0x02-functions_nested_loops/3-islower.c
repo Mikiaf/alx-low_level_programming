5458f97c3a25a39fae8850f19d6e436cb6adcf04
Mikiaf <mikiyasafework12@gmail.com>
2022-09-14 16:35:11 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - cheak lowercase characters
+ * @c: assied to the characters
+ *
+ * Return:0, 1
+ */
+int _islower(int c)
+{
+	return ((islower(c)));
+}
