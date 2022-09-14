b006509b3cfe8d8119ef57396515485f775502c3
Mikiaf <mikiyasafework12@gmail.com>
2022-09-14 15:34:21 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - pirnt the alphabet x10
+ *
+ * Return:0
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	int i;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 int _putchar(char c);
 int print(void);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 
 
