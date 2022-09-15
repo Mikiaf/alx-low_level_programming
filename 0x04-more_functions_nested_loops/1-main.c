dbaf3c3ae11af4038c5148fe0d16916258547126
Mikiaf <mikiyasafework12@gmail.com>
2022-09-15 13:07:10 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - calls function
+ *
+ *Return:0
+*/
+int main(void)
+{
+	char c;
+	char r;
+
+	c = '0';
+	r = _isdigit(c);
+	_putchar(c + ':');
+	_putchar(r);
+	return (0);
+}
