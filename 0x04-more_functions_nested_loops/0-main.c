631eb812e18fc3a731f55ba902c91e97ab0c326d
Mikiaf <mikiyasafework12@gmail.com>
2022-09-15 12:46:06 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - calls function
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char c;
+
+	c = _isupper('A');
+	_putchar(c);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - calls functions
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int c;
+
+	_putchar(c);
+	return(0);
+}
