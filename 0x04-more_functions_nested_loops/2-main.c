9a18ff650bf7ba0c221dba6c83f7bc88bb24be4e
Mikiaf <mikiyasafework12@gmail.com>
2022-09-16 14:33:13 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int i = 1024;
+	int j = 98;
+	int r;
+
+	r = mul(i, j);
+	_putchar(r);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: 1
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
