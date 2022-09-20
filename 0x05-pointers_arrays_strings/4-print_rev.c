3945a149d574763a156d1ab94dd81cace89d800f
Mikiaf <mikiyasafework12@gmail.com>
2022-09-20 12:18:36 -0700

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * print_rev - print string in reverse
  * @s: is pointer
@@ -8,7 +9,7 @@
  */
 void print_rev(char *s)
 {
-	int len = _strlen(s);
+	int len = strlen(s);
 
 	while (len--)
 		_putchar(*(s + len));
