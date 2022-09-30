fcd0b269e85d7ff175a71687b35b4cfdb3132a69
Mikiaf <mikiyasafework12@gmail.com>
2022-09-30 16:02:09 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the number arguments passed in
+ * @argc: is int
+ * @argv: is string
+ *
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d", argc);
+	return (0);
+}
