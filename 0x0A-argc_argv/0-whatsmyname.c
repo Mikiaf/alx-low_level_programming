d86c94b8f8e37ac095a4c8917f4081e545283622
Mikiaf <mikiyasafework12@gmail.com>
2022-09-30 15:56:42 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - command line agrement
+ * @argc: count string
+ * @argv: is string
+ *
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
