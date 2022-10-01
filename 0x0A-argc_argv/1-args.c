2b679f248debb0ee1422b20ac40e131ad0e15403
Mikiaf <mikiyasafework12@gmail.com>
2022-09-30 17:46:57 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,12 +4,12 @@
  * main- print the number arguments passed in
  * @argc: is int
  * @argv: is string
- *
+ *i:int
  * Return:0
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
