be22822a3743e6ccff76da3ad72a8c16e9973bca
Mikiaf <mikiyasafework12@gmail.com>
2022-09-30 18:09:08 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,18 +11,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int j;
+	int i, j;
 
 	if (argc == 3)
 	{
-		i = atoi(argv[1]);
-		j = atoi(argv[2]);
-		printf("%d", i * j);
-	}
-	else
-	{
-		printf("Error");
-	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	printf("%d\n", i * j);
 	return (0);
+	}
+	printf("Error\n");
+	return (1);
 }
