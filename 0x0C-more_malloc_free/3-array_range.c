e331309141175b0e27308cae0441498f2da487cd
Mikiaf <mikiyasafework12@gmail.com>
2022-10-06 19:50:09 -0700

    task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * array_range - function
+ * @min: int
+ * @max: int
+ *
+ * Return:0
+ */
+int *array_range(int min, int max)
+{
+	int i, l;
+	int *a;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	l = max - min + 1;
+	a = malloc(sizof(int) * 1);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < l; i++, min++)
+	{
+		a[i] = min;
+	}
+	return (a);
+}
