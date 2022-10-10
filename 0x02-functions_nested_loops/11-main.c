1f6a4e188adcdc6671145fbc2774538a709ae002
Mikiaf <mikiyasafework12@gmail.com>
2022-10-09 22:40:43 -0700

    task 11

--- 0x02-functions_nested_loops/11-main.c
+++ 0x02-functions_nested_loops/11-main.c
@@ -3,6 +3,6 @@
 
 int main(void)
 {
-	print_to_98(1);
+	print_to_98(-10);
 	return (0);
 }
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,25 +7,61 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
+	int i, j, sum;
 
 	j = 98;
 	if (j >= n)
 	{
 		for (i = n; i <= j; i++)
 		{
-			if (i < 10)
+			if (i < -99)
+			{
+				i *= -1;
+				sum = i / 10;
+				_putchar('-');
+				_putchar(sum / 10 + '0');
+				_putchar(sum % 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+				i *= -1;
+			}
+			else if (i < -9)
+			{
+				i *= -1;
+				_putchar('-');
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+				i *= -1;
+			}
+			else if (i < 0)
+			{
+				i *= -1;
+				_putchar('-');
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+				i *= -1;
+			}
+			else if (i < 10)
 			{
 				_putchar(i + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
+			else if (i == 98)
+			{
+				_putchar(98 / 10 + '0');
+				_putchar(98 % 10 + '0');
+			}
 			else
 			{
-				 _putchar(',');
-				 _putchar(' ');
 				_putchar(i / 10 + '0');
 				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 	}
@@ -48,7 +84,6 @@ void print_to_98(int n)
 			}
 			else
 			{
-				int sum;
 				sum = i / 10;
 
 				_putchar(sum / 10 + '0');
