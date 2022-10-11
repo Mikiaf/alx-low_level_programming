f01af9e840f0b24539ec966ca1e48148f92c2759
Mikiaf <mikiyasafework12@gmail.com>
2022-10-11 12:00:47 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - print string
+ * @d: struct pointer
+ * @name: string
+ * @age:float
+ * @owner:string
+ *
+ * Return:null
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
