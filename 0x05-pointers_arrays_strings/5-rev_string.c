7d25ca5db9a762838f1d309443b7052b3d45144e
Mikiaf <mikiyasafework12@gmail.com>
2022-10-11 04:47:36 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverses strings
+ * @s: string
+ *
+ * Return:null
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i; s[j] >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ void print_rev(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+void rev_string(char *s);
 
 
 #endif
