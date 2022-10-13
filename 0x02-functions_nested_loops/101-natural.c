69da990565c7c0879c7eb39798e00acdf056385b
Mikiaf <mikiyasafework12@gmail.com>
2022-10-13 04:12:22 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function that return sum
+ * i:int
+ * sum:sum of numbers
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+return (0);
+}
