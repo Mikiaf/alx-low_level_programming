dcd1d604d096caad19e1e41f0c676d345936b033
Mikiaf <mikiyasafework12@gmail.com>
2022-10-13 05:49:56 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print fibonacci
+ * i:int
+ * j:int
+ * sum:sum of int
+ *
+ * Return:0;
+ */
+int main(void)
+{
+	int i;
+	int j = 2;
+	int sum;
+
+	printf("1, 2,");
+	for (i = 1; i <=50; i++)
+	{
+		j += i;
+		printf("%d, ", j);
+	}
+	printf("\n");
+	return (0);
+}
