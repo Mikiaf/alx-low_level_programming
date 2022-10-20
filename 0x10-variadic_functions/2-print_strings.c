1246eb384acd8fcad080748a248067897d6b8a5c
Mikiaf <mikiyasafework12@gmail.com>
2022-10-20 01:42:31 -0700

    task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_strings - print string
+ * @separator: separate string
+ * @n: number of args
+ * Return:0
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list string;
+	va_start(string, n);
+	for (i = 0; i < n; i++)
+	{
+		char *s = va_arg(string, char*);
+		if(s == NULL)
+		{
+			printf("nil");
+			if(i < n - 1 && separator != NULL)
+			{
+				printf("%s",separator);
+			}
+		}
+		else
+		{
+			printf("%s", s);
+			if(i < n - 1 && separator != NULL)
+			{
+				printf("%s",separator);
+			}
+		}
+	}
+	printf("\n");
+}
