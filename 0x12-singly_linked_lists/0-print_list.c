5daab5941f4bc19f342340f3bc66d97f2d4f88bd
Mikiaf <mikiyasafework12@gmail.com>
2022-10-20 14:12:33 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,12 @@
 #include "lists.h"
-/** print_list - prints all the elements of a list
+
+
+/**
+ * print_list - prints all the elements of a list
  * @h: list
  *
  * Return: number
- */
+*/
 size_t print_list(const list_t *h)
 {
 	size_t n;
