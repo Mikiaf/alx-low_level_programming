33286e8e643eae5eb3b2bc8205ce62bdbf8955e1
Mikiaf <mikiyasafework12@gmail.com>
2022-10-20 14:25:39 -0700

    task 1

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - adds node
+ * @head: list
+ * @str:string
+ *
+ * Return address
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	size_t n;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		;
+
+	new_node->len = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
