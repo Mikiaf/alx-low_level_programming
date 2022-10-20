562c885a1ee794e9734f5217c2addfcc9c141685
Mikiaf <mikiyasafework12@gmail.com>
2022-10-20 14:50:45 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * text - excuted
+ *
+ * Return:null
+ */
+void __attribute__((constructor)) text()
+{
+	printf("you're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
