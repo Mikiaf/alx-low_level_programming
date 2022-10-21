3aff966d7ce435a760685c6a6fd9bbc8a963e83e
Mikiaf <mikiyasafework12@gmail.com>
2022-10-21 14:11:44 -0700

    task 2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,13 +10,14 @@ char *_strdup(char *str)
 {
 	int a = 0, i = 1;
 	char *s;
+
 	if (str == NULL)
 		return (NULL);
 	while (str[i])
 	{
 		i++;
 	}
-	s = malloc((sizeof(char) * i) +i);
+	s = malloc((sizeof(char) * i) + i);
 	if (s == NULL)
 		return (NULL);
 
