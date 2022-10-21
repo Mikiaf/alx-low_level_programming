98fff741c5e1affb2a23bc09972714e316bd0e12
Mikiaf <mikiyasafework12@gmail.com>
2022-10-21 14:02:59 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	a = malloc(sizeof(char) * size);
 	if (a == NULL)
 		return (NULL);
-	for (i = 0 i < size; i++)
-		array[i] = c;
+	for (i = 0; i < size; i++)
+		a[i] = c;
 	return (a);
 }
