890e2c0a038d18eee0c76ab426ee08de8ffdde25
Mikiaf <110899412+Mikiaf@users.noreply.github.com>
2022-10-22 02:29:41 -0700

    Create 100-times_table.c
    task  12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "mian.h"
+/**
+* print_times_table - print times table
+* @n: int
+*
+* Return: void
+*/
+void print_times_table(int n)
+{
+	int i, j, sum;
+
+	if (n < 0 || n > 98)
+		_putchar(' ');
+	for (i = 0; i <= n; i++)
+	{
+		for(j = 0; j <= n; j++)
+		{
+			sum = j * i;
+			if(sum > 9)
+			{
+				if( j == n)
+				{
+					_putchar(' ');
+					_putchar(sum / 10 + '0');
+					_putchar(sum % 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(sum / 10 + '0');
+					_putchar(sum % 10 + '0');
+					_putchar(',');
+				}
+			}
+			else
+			{
+				if(j == n)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(sum + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(sum + '0');
+					_putchar(',');
+				}
+			}
+		}
+		_putchar('\n');
+	}	
+}
