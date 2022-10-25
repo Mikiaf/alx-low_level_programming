d45e59e9c1520fe9e9857698f7b8815b43c5b72c
Mikiaf <mikiyasafework12@gmail.com>
2022-10-25 11:13:32 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - print all the elements
+ * @h:linked list
+ *
+ * Return:number of node
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++
+	}
+	return (i);
+}
