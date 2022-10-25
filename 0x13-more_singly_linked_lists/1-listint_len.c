bac070e094f98f10f564e05bc3cfefcc5cdf5fb5
Mikiaf <110899412+Mikiaf@users.noreply.github.com>
2022-10-25 12:50:01 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* listint_len - print the number of elements 
+* @h: pointer
+*
+* Return: number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+        size_t i;
+        
+        while (h)
+        {
+          i++;
+          h = h->next;
+        }
+    return (i);
+}
